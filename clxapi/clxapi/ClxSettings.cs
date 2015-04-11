3ac47308359dce14e65075a979400742ab5464ed
Grenmyr <dg222cs@student.lnu.se>
2015-04-11 16:43:52 +0200

    Add comments for ClxSettings.cs

--- clxapi/clxapi/ClxSettings.cs
+++ clxapi/clxapi/ClxSettings.cs
@@ -6,22 +6,39 @@ using System.Text;
 namespace clxapi
 {
     /// <summary>
-    /// Containts all paths toward ClxApi
+    /// Containts all paths and params toward Clx Api
     /// </summary>
     public class ClxSettings 
     {
 
         private string baseURI = "https://clx-aws.clxnetworks.com/api";
+
+        /// <summary>
+        /// Property change baseURI
+        /// </summary>
         public string BaseURI 
         {
             get { return baseURI; }
             set { baseURI = value; }
         }
-
+        /// <summary>
+        /// Path to operator toward Clx api.
+        /// </summary>
         public const string OperatorPath = "/operator/";
+
+        /// <summary>
+        /// Path to gateway toward Clx api.
+        /// </summary>
         public const string GatewayPath = "/gateway/";
+
+        /// <summary>
+        /// Path to price toward Clx api.
+        /// </summary>
         public const string PricePath = "/price/";
 
+        /// <summary>
+        /// Param for date toward Clx api..
+        /// </summary>
         public const string DateParam = "/date=";
 
     }
