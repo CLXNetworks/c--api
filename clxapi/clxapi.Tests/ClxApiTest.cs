041c0334ef422fb07346688f3ce1197f5012d7e5
Grenmyr <dg222cs@student.lnu.se>
2015-04-11 16:11:36 +0200

    Removed old tests

--- clxapi/clxapi.Tests/ClxApiTest.cs
+++ clxapi/clxapi.Tests/ClxApiTest.cs
@@ -11,45 +11,6 @@ namespace clxapi.Tests
     [TestClass]
     public class ClxApiTests
     { 
-        
-        [TestMethod]
-        public void GetTest()
-        {
-            var dev = new DeveloperSettings();
-
-            var clxApi = new ClxApi(new string[] { dev.Username, dev.Password });
-            //var data = clxApi.GetGatewayById(2182);
-            //var data = clxApi.GetGateways();
-            //var data = clxApi.GetOperatorById(10);
-            var data = clxApi.GetPriceEntriesByGatewayId(2182);
-            //var data = clxApi.GetPriceEntriesByGatewayIdAndOperatorId(2182, 8);
-            //var data = clxApi.GetPriceEntriesByGatewayIdAndOperatorIdAndDate(2182, 8, new DateTime(2012,01,01));
-            Assert.IsInstanceOfType(data, typeof(JArray));
-            Assert.IsNotNull(data);
-        
-        }
-        [TestMethod]
-        [ExpectedException(typeof(AuthenticationException))]
-        public void GetOperatorTestAuthenticationWrongPassword()
-        {
-            var dev = new DeveloperSettings();
-
-            var clxApi = new ClxApi(new string[] { dev.Username, "wrongPassword" });
-            var data = clxApi.GetOperators();
-            Assert.IsNull(data);
-
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(AuthenticationException))]
-        public void GetOperatorTestAuthenticationWrongUsername()
-        {
-            var dev = new DeveloperSettings();
-
-            var clxApi = new ClxApi(new string[] { "wrongUserName", dev.Password });
-            var data = clxApi.GetOperators();
-            Assert.IsNull(data);
-        } 
-        // TODO: Add more testing
+        // TODO: Add testing
     }
 }
