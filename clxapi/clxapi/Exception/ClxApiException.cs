42dd509a378f135d46a43b26d0589e384ac37317
Grenmyr <dg222cs@student.lnu.se>
2015-04-22 14:18:31 +0200

    ADD created two new custom Exception, one for handle errors from Clx Api, other to catch wrapper exceptions. Note: They are not implemented, just created.

---
+++ clxapi/clxapi/Exception/ClxApiException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace clxapi
+{
+    /// <summary>
+    /// TODO: Implement custom exception to handle api errors.
+    /// </summary>
+    public class ClxApiException : Exception
+    {
+    }
+}
---
+++ clxapi/clxapi/Exception/ClxException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace clxapi
+{
+    /// <summary>
+    /// Class to handle exception from Library
+    /// </summary>
+    public class ClxException : Exception
+    {
+
+        /// <summary>
+        /// TODO: Implement Custom Exception class.
+        /// </summary>
+        /// <param name="message"></param>
+        public ClxException (string message)
+            : base(message)
+        {}
+    }
+}
