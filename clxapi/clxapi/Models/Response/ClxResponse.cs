060634de87914904148f108e6dd547c5e978f0a0
Grenmyr <dg222cs@student.lnu.se>
2015-04-22 14:27:34 +0200

    Add Created ClxResponse class that HttpAdapter use to bind and return data from clx api.

---
+++ clxapi/clxapi/Models/Response/ClxResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace clxapi
+{
+    /// <summary>
+    /// Class with properties to return response data.
+    /// </summary>
+    public class ClxResponse
+    {
+        /// <summary>
+        /// Property: Contains HttpStatuscode.
+        /// </summary>
+        public int StatusCode { get; set; }
+
+        /// <summary>
+        /// Property: Contains header values.
+        /// </summary>
+        public string Header { get; set; }
+
+        /// <summary>
+        /// Property: Contains body values.
+        /// </summary>
+        public string Body { get; set; }
+
+        /// <summary>
+        /// Property: Contains errormessage.
+        /// </summary>
+        public string Error { get; set; }
+    }
+}
